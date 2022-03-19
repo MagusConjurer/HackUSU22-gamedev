bd18abaf424ae6e904c94f6099a8c6dfad73ce6b
noahyoda <noahyoda1@gmail.com>
2022-03-19 01:54:05 -0600

    fixed

--- HackUSU22/Assets/Scripts/BackgroundController.cs
+++ HackUSU22/Assets/Scripts/BackgroundController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-
+    public GameObject player;
     private Rigidbody2D rb;
     private BoxCollider2D backCollider;
     private float width;
@@ -17,23 +17,22 @@ public class BackgroundController : MonoBehaviour
 
         width = backCollider.size.x;
         backCollider.enabled = false;
-
-        TileGeneration.generateGround(transform, width);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.velocity = new Vector2(PlayerController.getDirection(), 0);
-        // if below/above outer threshold tp image to reset position
-        if(transform.position.x < -width)
+        float playerXDir = -player.GetComponent<Rigidbody2D>().velocity.x;
+        if (rb.velocity.x != playerXDir)
+            rb.velocity = new Vector2(playerXDir, 0);
+
+        // if beyond outer threshold tp image to reset position
+        if (transform.position.x < -width)
         {
             Vector2 resetPos = new Vector2(2f * width, 0);
             transform.position = (Vector2)transform.position + resetPos;
+
         }
-        foreach(GameObject o in TileGeneration.tiles)
-        {
-            o.transform.position = new Vector3(transform.position.x+38, -2);
-        }
+
     }
 }
