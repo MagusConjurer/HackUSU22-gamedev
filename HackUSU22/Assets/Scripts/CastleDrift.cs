d987c39ce38a36f56f099523f213ab91c519746c
Noah <noahyoda1@gmail.com>
2022-03-19 06:59:42 -0600

    new mountain baby

---
+++ HackUSU22/Assets/Scripts/CastleDrift.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleDrift : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public BoxCollider2D box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rb.velocity = new Vector2(-4f, 0);
+    }
+}
