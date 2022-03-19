24739d17a02e477d2d95dcbd239c72cb4bc11993
MagusConjurer <cameronisadavis@gmail.com>
2022-03-18 20:08:21 -0600

    Player can move left to right and jump

--- HackUSU22/Assets/Scripts/PlayerController.cs
+++ HackUSU22/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 10;
     public float jumpForce;
-    public Camera mainCam;
+    public LayerMask ground;
 
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
+    private Animator anim;
 
     private float dirX;
 
@@ -23,15 +24,16 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        dirX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2 (dirX * moveSpeed * Time.deltaTime, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -43,12 +45,21 @@ public class PlayerController : MonoBehaviour
     {
         if (dirX > 0.1f)
         {
-            // Set anim state
+            // Set anim state for walk animation
             sprite.flipX = false;
         }
         else if (dirX < 0.1f)
         {
-            // Set anim state
+            // Slower back step?
         }
+        else
+        {
+            // Set anim idle
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, ground);
     }
 }
