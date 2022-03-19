40fe02f00c9d3f0fd0e86e1e3363621d32d7dd50
MagusConjurer <cameronisadavis@gmail.com>
2022-03-19 04:54:39 -0600

    Update player prefab and add camera offset.

--- HackUSU22/Assets/Scripts/CameraController.cs
+++ HackUSU22/Assets/Scripts/CameraController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
+    public float cameraOffsetY = 10;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y + cameraOffsetY, transform.position.z);
     }
 }
 
