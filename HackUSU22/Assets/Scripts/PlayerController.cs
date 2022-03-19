c1f1af0277cff86f89473bc02071d10e7a0e6dd7
Adam <atcolton@tutanota.com>
2022-03-19 06:09:17 -0600

    modified:   HackUSU22/Assets/Scripts/PlayerController.cs

--- HackUSU22/Assets/Scripts/PlayerController.cs
+++ HackUSU22/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 //using System.Numerics;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : BaseEntity
 {
-    private AudioClip scream = Resources.Load<AudioClip>("Audio/scream");
+    private AudioClip scream;
+    private AudioClip[] clashes;
 
     protected override void OnStart() {
         jumpForce = 10;
         maxHealth = 100;
+        scream = Resources.Load<AudioClip>("Audio/scream");
+        clashes = new AudioClip[] {
+         Resources.Load<AudioClip>("Audio/clash1"),
+         Resources.Load<AudioClip>("Audio/clash2"),
+         Resources.Load<AudioClip>("Audio/clash3"),
+         Resources.Load<AudioClip>("Audio/clash4"),
+         Resources.Load<AudioClip>("Audio/clash5"),
+        };
+    }
+
+    private void PlayRandomClash() {
+        var r = new System.Random();
+        AudioClip c = clashes[r.Next(1,5)];
+        sndSource.PlayOneShot(c);
     }
 
     protected override Vector2 GetDecision() {
